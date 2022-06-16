0772175c72d3c353ed5ad0c532f88709a488f50d
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-06-16 16:46:25 +0900

    state 변수

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let title = "제주도 여행 후기";
-
+  let [title, setTitle] = useState([
+    "제주도 여행 후기",
+    "부산 여행 후기",
+    "강릉 여행 후기",
+  ]);
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Sungwk's BLOG</h4>
       </div>
-      <h4 style={{ fontWeight: "bold", fontSize: "20px" }}>{title}</h4>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>글 내용</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>글 내용</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>글 내용</p>
+      </div>
     </div>
   );
 }
