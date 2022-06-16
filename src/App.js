53115dec76fbf08d5128b3e1c19d88fac5a51931
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-06-16 16:29:50 +0900

    header

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let title = "제주도 여행 후기";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Sungwk's BLOG</h4>
+      </div>
+      <h4 style={{ fontWeight: "bold", fontSize: "20px" }}>{title}</h4>
     </div>
   );
 }
