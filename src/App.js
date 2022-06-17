914380332b3b31c0bda282e3f3b705030668851b
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-06-17 15:08:34 +0900

    모달창

--- src/App.js
+++ src/App.js
@@ -10,12 +10,30 @@ function App() {
   ]);
 
   let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Sungwk's BLOG</h4>
       </div>
+      <button
+        onClick={() => {
+          let arr = [...title];
+          arr[0] = "서울 여행 후기";
+          setTitle(arr);
+        }}
+      >
+        글수정
+      </button>
+      <button
+        onClick={() => {
+          let arr = [...title].sort();
+          setTitle(arr);
+        }}
+      >
+        글 제목 정렬
+      </button>
       <div className="list">
         <h4>
           {title[0]}
@@ -29,22 +47,37 @@ function App() {
           {like}
         </h4>
         <p>글 내용</p>
-        <span
-          onClick={() => {
-            setTitle(["서울 여행 후기", "부산 여행 후기", "강릉 여행 후기"]);
-          }}
-        >
-          oo
-        </span>
       </div>
       <div className="list">
         <h4>{title[1]}</h4>
         <p>글 내용</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4
+          onClick={() => {
+            if (modal == false) {
+              setModal(true);
+            } else {
+              setModal(false);
+            }
+          }}
+        >
+          {title[2]}
+        </h4>
         <p>글 내용</p>
       </div>
+
+      {modal == true ? <Modal /> : ""}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
